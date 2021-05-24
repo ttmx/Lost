90e5dc9b2ed5b4176c60000fc2f4e734fe8d7ace
Tiago Teles <tiago.sequeira.teles@gmail.com>
2021-05-24 03:24:01 +0100

    Actually finished main and did headers for helper class

---
+++ src/Lost.java
@@ -0,0 +1,9 @@
+public class Lost {
+    public Lost(byte[][] grid, byte[] wheelLocs, int[] wheelTimes, byte[] positions){
+
+    }
+
+    public int[] result() {
+        return null;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,26 @@ public class Main {
             byte height = (byte) in.nextInt();
             byte width = (byte) in.nextInt();
             byte[][] grid = new byte[height][width];
-            byte wheels = (byte) in.nextInt();
+            byte numWheels = (byte) in.nextInt();
+            byte[] wheelLocs = new byte[numWheels*2];
+            int[] wheelTimes = new int[numWheels];
             for (int h = 0;h<height;h++){
                 for (int w = 0;w<width;w++){
                     grid[h][w] = in.nextByte();
                 }
-                assert (in.nextByte() == '\n');
+                in.nextByte();
             }
+            for(byte w = 0;w<numWheels;w++){
+                wheelLocs[w*2] = (byte) in.nextInt();
+                wheelLocs[w*2+1] = (byte) in.nextInt();
+                wheelTimes[w] = in.nextInt();
+            }
+            byte[] positions = new byte[4];
+            for(int p = 0;p<4;p++) {
+                positions[p] = (byte) in.nextInt();
+            }
+            Lost l = new Lost(grid,wheelLocs,wheelTimes,positions);
+            int[] result = l.result();
         }
     }
 }
